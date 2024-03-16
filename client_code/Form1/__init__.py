9d875c823084566d82092a72278628137aca7faa
ppendant1@gmail.com <ppendant1@gmail.com>
2024-03-16 22:53:27 +0000

    Created Снимок22.JPG and ServerModule1; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,27 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def label_3_show(self, **event_args):
+    """This method is called when the Label is shown on the screen"""
+    pass
+
+  def button_10_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+from ._anvil_designer import Form1Template
+from anvil import *
+import anvil.server
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def label_3_show(self, **event_args):
+    """This method is called when the Label is shown on the screen"""
+    pass
+
